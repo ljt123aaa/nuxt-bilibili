5bc306fbd64becbc148648bf1a4598dfa7c77a1f
ljt <626359533@qq.com>
2024-05-28 13:00:46 +0800

    34546

--- app.vue
+++ app.vue
@@ -4,6 +4,7 @@ useSeoMeta({
   title: '哔哩哔哩 (゜-゜)つロ 干杯~-bilibili',
   description: '哔哩哔哩(bilibili.com)是国内知名的视频弹幕网站,这里有及时的动漫新番,活跃的ACG氛围,有创意的Up主。大家可以在这里找到许多欢乐。',
   keywords: '哔哩哔哩,bilibili,bilibili.com,哔哩哔哩弹幕网,弹幕网,弹幕,b站,bilibili弹幕,弹幕网站,弹幕网',
+  // 绕开B站的防盗链
   referrer: 'no-referrer'
 });
 // let active = ref(0);
